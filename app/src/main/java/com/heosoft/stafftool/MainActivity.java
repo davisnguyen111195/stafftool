c632c7820ba5ea4da611c09b19faa1a9d30c7574
datnt <nguyendat111195@gmail.com>
2023-12-19 16:19:36 +0700

    Add view

--- app/src/main/java/com/heosoft/stafftool/MainActivity.java
+++ app/src/main/java/com/heosoft/stafftool/MainActivity.java
@@ -1,8 +1,13 @@
 package com.heosoft.stafftool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
+
+    }
+
+    public void setupView() {
+        EditText addName = findViewById(R.id.addName);
+        EditText addAddress = findViewById(R.id.addAddress);
+        EditText addAge = findViewById(R.id.addAge);
+        Switch addSex = findViewById(R.id.addSex);
+        Button btn_Add = findViewById(R.id.btn_Add);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
     }
 }
